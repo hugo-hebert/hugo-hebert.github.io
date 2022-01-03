693351bd123f8fc3d8712c71b88405bed8ce721e
hugo hebert <hugo.mike.hebert@gmail.com>
2022-01-03 17:51:34 +1300

    changed to lodash get in header component

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,14 +1,20 @@
 
+import get from 'lodash.get';
+
 const Header = ({data}) => {
+  
+  const title = get(data, 'attributes.title', '');
+  const navigation = get(data, 'attributes.navigation', []);
+
    
   return (
     <header className="main-nav">
       <a href="/" className="barebones">
-        <h3 className="main-nav--title">{data.attributes.title}</h3>
+        <h3 className="main-nav--title">{title}</h3>
       </a>
       <div className="main-nav--links">
       {
-        data.attributes.navigation.map( (nav, ix) => {
+        navigation.map( (nav, ix) => {
           return (
             <a href={nav.url} key={ix}>
               {nav.display_value}
